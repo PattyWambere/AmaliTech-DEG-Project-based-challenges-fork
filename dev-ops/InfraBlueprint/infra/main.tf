609e0e6e00494d499a533572b62fe7a21903b01a
NIYIGENA Patrick <pattywrld2003@gmail.com>
2026-04-26 15:41:40 +0200

    feat(storage): add S3 bucket with versioning and access controls

--- dev-ops/InfraBlueprint/infra/main.tf
+++ dev-ops/InfraBlueprint/infra/main.tf
@@ -296,3 +296,34 @@ resource "aws_db_instance" "main" {
     Project = "vela-payments"
   }
 }
+
+
+# ─────────────────────────────────────────────
+# PART 4 — STORAGE
+# ─────────────────────────────────────────────
+
+resource "aws_s3_bucket" "assets" {
+  bucket = var.s3_bucket_name
+
+  tags = {
+    Name    = "vela-static-assets"
+    Project = "vela-payments"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "assets" {
+  bucket = aws_s3_bucket.assets.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "assets" {
+  bucket = aws_s3_bucket.assets.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
